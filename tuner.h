1a94b25f0d85cdbb91a546b9b2b5fb8a54bc209b
lz1abc <ch.levkov@gmail.com>
2021-03-10 22:39:34 +0100

    ....

---
+++ tuner.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#define C_MAX  (0xfff);
+#define L_MAX  (0xfff);
+
+struct relays {
+	int c; // variable capacitor
+	int l; // variable inductance
+	bool c2; // enabled or disables capacitor
+};
+
+void command_set_relays(struct relays, char *data);
+void command_set_tune(bool bTune, char *data);
+void command_hi(char* data);
+void command_request_adc(char *data);
+
+void data_handler(char *data);
+
+int get_forward_v();
+int get_backward_v();
+int get_antenna_v();
+int get_powersupply_v();
+float get_swr();
+
+
+
+
+
